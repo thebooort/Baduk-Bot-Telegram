84f2ab598db7fce65028e2eeda61333bfb63652f
Enrique Casale Linde <kicalin@protonmail.com>
2018-07-12 12:16:51 +0200

    Renderiza el tablero
    Habra que retocarlo para añadirle comentarios y convertirlo en una clase

---
+++ render.py
@@ -0,0 +1,34 @@
+import re
+from PIL import Image, ImageDraw, ImageFont, ImageFilter
+
+def grid_to_coords(grid):
+    grid_size = (40,40)
+    coords = [grid[i] * grid_size[i] for i in range(2)] * 2
+    coords[2] += grid_size[0]
+    coords[3] += grid_size[1]
+    return coords
+
+#Draw the board
+grid_size = (40,40)
+board = Image.new('RGB', [10*x for x in grid_size], (255,204,153))
+
+for i in range(1,10):
+    i = i * 40
+    for j in range(1,10):
+        j = j * 40
+        ImageDraw.Draw(board).line([i,j,i,40] , width=1)
+        ImageDraw.Draw(board).line([i,j,40,j] , width=1)
+
+
+lista_de_letras =  ['A','B','C','D','E','F','G','H','J']
+lista_de_numeros = ['1','2','3','4','5','6','7','8','9']
+
+for i in range(0,9):
+    letra = lista_de_letras[i]
+    ImageDraw.Draw(board).text((20,1.14*(i+1)*34),letra,fill=(0,0,0))
+
+for i in range(0,9):
+    numero = lista_de_numeros[i]
+    ImageDraw.Draw(board).text((1.14*(i+1)*35,20),numero,fill=(0,0,0))
+
+board.show()

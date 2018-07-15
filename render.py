08989b903fe215f7a4dbbb3169674cbdbdab89a4
Enrique Casale Linde <kicalin@protonmail.com>
2018-07-15 13:06:23 +0200

    Render class added
    Python class for render 3 different boards (9x9,13x13,19x19). This class still need a method to draw the stones giving their position in the matrix.

--- render.py
+++ render.py
@@ -1,42 +1,115 @@
 import re
 from PIL import Image, ImageDraw, ImageFont, ImageFilter
 
-def grid_to_coords(grid):
-    grid_size = (40,40)
-    coords = [grid[i] * grid_size[i] for i in range(2)] * 2
-    coords[2] += grid_size[0]
-    coords[3] += grid_size[1]
-    return coords
+class Render():
 
-#Draw the board
-grid_size = (40,40)
-grid_size_1 = 40
-# board = Image.new('RGB', [10*x for x in grid_size], (255,204,153))
+    #Inicialiaze the Render class with an empty board
+	#Tablero can have 3 values, 0,1,2. 0 for 9x9, 1 for 12x12 and 2 for 18x18
+	def __init__(self, turn=True,tablero=0):
+		self.turn = turn
+		self.grid_size = (40,40)
+		if(tablero==0):
+			self.board = self.create_board_9()
+		elif(tablero==1):
+			self.board = self.create_board_13()
+		else:
+			self.board = self.create_board_19()
+		#self.piece_locations =
 
-# Open wood board image and crops it to the desired size 
-image_source = Image.open("images/wood_board.jpg")
-area = (0, 0, grid_size_1*10, grid_size_1*10) # (initial x, initial y, width, height)
-board = image_source.crop(area)
+    #Create an empty 9x9 Go board
+	def create_board_9(self):
+
+        #board = Image.new('RGB', [10*x for x in self.grid_size], (255,204,153))
+		image_source = Image.open("images/wood_board.jpg")
+		area = (0, 0, grid_size_1*10, grid_size_1*10) # (initial x, initial y, width, height)
+		board = image_source.crop(area)
+		format = 40
+		for i in range(1,10):
+			i = i * format
+			for j in range(1,10):
+				j = j * format
+				ImageDraw.Draw(board).line([i,j,i,40] , width=1,fill=(0,0,0))
+				ImageDraw.Draw(board).line([i,j,40,j] , width=1,fill=(0,0,0))
+
+
+		lista_de_letras =  ['A','B','C','D','E','F','G','H','J']
+		lista_de_numeros = ['1','2','3','4','5','6','7','8','9']
+
+		for i in range(0,9):
+			letra = lista_de_letras[i]
+			ImageDraw.Draw(board).text((20,1.14*(i+1)*34),letra,fill=(0,0,0))
+
+		for i in range(0,9):
+			numero = lista_de_numeros[i]
+			ImageDraw.Draw(board).text((1.14*(i+1)*35,20),numero,fill=(0,0,0))
+
+		return board
+
+	#Create an empty 13x13 Go board
+	def create_board_13(self):
+
+		board_size = 14 #Always one more than the real board size
+		image_source = Image.open("images/wood_board.jpg")
+		area = (0, 0, self.grid_size[0]*board_size, self.grid_size[0]*board_size) # (initial x, initial y, width, height)
+		board = image_source.crop(area)
+
+		for i in range(1,board_size):
+		    i = i * self.grid_size[0]
+		    for j in range(1,board_size):
+		        j = j * self.grid_size[0]
+		        ImageDraw.Draw(board).line([i,j,i,self.grid_size[0]] , width=1,fill=(0,0,0))
+		        ImageDraw.Draw(board).line([i,j,self.grid_size[0],j] , width=1,fill=(0,0,0))
+
+		lista_de_letras =  ['A','B','C','D','E','F','G','H','J','K','L','M','O']
+		lista_de_numeros = ['1','2','3','4','5','6','7','8','9','10','11','12','13']
+
+		for i in range(0,board_size-1):
+		    letra = lista_de_letras[i]
+		    ImageDraw.Draw(board).text((20,1.15*(i+1)*34),letra,fill=(0,0,0))
+
+		for i in range(0,board_size-1):
+		    numero = lista_de_numeros[i]
+		    ImageDraw.Draw(board).text((1.14*(i+1)*35,20),numero,fill=(0,0,0))
+
+		return board
+
+	#Create an empty 19x19 Go board
+	def create_board_19(self):
+		#board = Image.new('RGB', [10*x for x in self.grid_size], (255,204,153))
+		image_source = Image.open("images/wood_board.jpg")
+		board_size = 20
+		area = (0, 0, self.grid_size[0]*board_size, self.grid_size[0]*board_size) # (initial x, initial y, width, height)
+		board = image_source.crop(area)
+		format = 40
+		for i in range(1,board_size):
+			i = i * format
+			for j in range(1,board_size):
+				j = j * format
+				ImageDraw.Draw(board).line([i,j,i,format] , width=1,fill=(0,0,0))
+				ImageDraw.Draw(board).line([i,j,format,j] , width=1,fill=(0,0,0))
+
+
+		lista_de_letras =  ['A','B','C','D','E','F','G','H','J','K','L','M','N','O','P','Q','R','S','T']
+		lista_de_numeros = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19']
+		list_size = board_size-1
+		for i in range(0,list_size):
+			letra = lista_de_letras[i]
+			ImageDraw.Draw(board).text((20,1.14*(i+1)*34),letra,fill=(0,0,0))
+
+		for i in range(0,list_size):
+			numero = lista_de_numeros[i]
+			ImageDraw.Draw(board).text((1.14*(i+1)*35,20),numero,fill=(0,0,0))
+
+		return board
+
+	def show_board(self):
+		self.board.show()
 
 
 
-for i in range(1,10):
-    i = i * 40
-    for j in range(1,10):
-        j = j * 40
-        ImageDraw.Draw(board).line([i,j,i,40] , width=1)
-        ImageDraw.Draw(board).line([i,j,40,j] , width=1)
 
 
-lista_de_letras =  ['A','B','C','D','E','F','G','H','J']
-lista_de_numeros = ['1','2','3','4','5','6','7','8','9']
 
-for i in range(0,9):
-    letra = lista_de_letras[i]
-    ImageDraw.Draw(board).text((20,1.14*(i+1)*34),letra,fill=(0,0,0))
+Tablero = Render(tablero=1)
 
-for i in range(0,9):
-    numero = lista_de_numeros[i]
-    ImageDraw.Draw(board).text((1.14*(i+1)*35,20),numero,fill=(0,0,0))
-
-board.show()
+Tablero.show_board()
